f4e69b23e8f533a04f354640f01871c40ef4273f
mariaclaralis <ferreiramariaclaralisboa@gmail.com>
2025-04-16 09:44:28 -0300

    fydgff

---
+++ atv3/Program.cs
@@ -0,0 +1,36 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Digite sua idade:");
+
+        string entrada = Console.ReadLine();
+
+
+        bool numero = true;
+
+        foreach (char c in entrada)
+        {
+            if (!Char.IsDigit(c))
+            {
+                numero = false;
+                break;
+            }
+        }
+
+        if (numero)
+        {
+            int idade = Convert.ToInt32(entrada);
+
+            if (idade >= 16)
+            {
+                Console.WriteLine("Vc pode votar.");
+            }
+            else
+            {
+                Console.WriteLine("Vc n pode votar.");
+            }
+        }
+
+    }
+}
