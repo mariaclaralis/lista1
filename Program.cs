4ae30ae90f46d2ba5d76f4129ee611f3910d61c6
mariaclaralis <ferreiramariaclaralisboa@gmail.com>
2025-04-16 08:54:59 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿class Program
+{
+    static void Main()
+    { 
+        Console.WriteLine("Digite um número:");
+        string entrada = Console.ReadLine();
+      
+        int numero = Convert.ToInt32(entrada);  
+        if (numero > 0)
+        {
+            Console.WriteLine("O número é positivo.");
+        }
+        else if (numero < 0)
+        {
+            Console.WriteLine("O número é negativo.");
+        }
+        else
+        {
+            Console.WriteLine("O número é zero.");
+        }
+    }
+}
