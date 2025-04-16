d84d2e2dadf7833f1e47c1d0361383ac7e077dc6
mariaclaralis <ferreiramariaclaralisboa@gmail.com>
2025-04-16 09:39:54 -0300

    oii

---
+++ atv2/Program.cs
@@ -0,0 +1,22 @@
+﻿class Program
+{
+    static void Main()
+    {
+        
+        Console.WriteLine("Digite um número:");
+
+        
+        string entrada = Console.ReadLine();
+
+        int numero = Convert.ToInt32(entrada);
+
+        if (numero % 2 == 0)
+        {
+            Console.WriteLine("O número é par.");
+        }
+        else
+        {
+            Console.WriteLine("O número é ímpar.");
+        }
+    }
+}
